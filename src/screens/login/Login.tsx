09c9c493a7c32832ac92df5a5e48f59dc1b143af
VladBrendak <brendakvlad@gmail.com>
2024-02-14 20:08:26 +0200

    minor fixes

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -1,44 +1,86 @@
-import React from 'react';
-import styles from './Login.module.css'
-import PopUp from '../../components/ui/popup/PopUp';
-import { useState } from 'react';
+import React from "react";
+import styles from "./Login.module.css";
+import PopUp from "../../components/ui/popup/PopUp";
+import { User } from "../../interfaces/User";
+import { useState } from "react";
 
 const Login = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [email, setEmail] = useState<string>('');
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [userData, setUserData] = useState<User>();
 
-    const PrintEmailINColsole = (e:any) => {
-        e.preventDefault();
-        console.log(email);
-    }
+  const clearUserData = {
+    name: "",
+    email: "",
+    password: "",
+  };
 
-    const openPopup: () => void = () => {
-        setIsOpen(true);
-    };
-      
-    const closePopup: () => void = () => {
-        setIsOpen(false);
-    };
+  const createUser = (e: any) => {
+    e.preventDefault();
+    console.log();
+  };
 
-    return (
-        <>
-            <div>
-                <div className={styles.loginContainer}>
-                    <h1>Login</h1>
-                    <form>
-                        <label htmlFor="Email"> Email </label>
-                        <input type="Email" onChange={e => setEmail(e.target.value)} value={email}/>
-                    </form>
-                    <button onClick={openPopup}>Open Popup</button>
-                    <button onClick={PrintEmailINColsole}> Print Email in console </button>
-                    {email !== '' && <p> Your email is: {email}</p>}
-                </div>
-            </div>
-            <div className={`${isOpen ? styles.popupEnterActive : styles.popupExitActive}`}>
-                <PopUp open={isOpen} onClose={closePopup}/>
-            </div>
-        </>
-    )
-}
+  const PrintEmailINColsole = (e: any) => {
+    e.preventDefault();
+    console.log(email);
+  };
+
+  const openPopup: () => void = () => {
+    setIsOpen(true);
+  };
+
+  const closePopup: () => void = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <div>
+        <div className={styles.loginContainer}>
+          <h1>Login</h1>
+          <form>
+            <label htmlFor="Username"> Username </label>
+            <input
+              type="Username"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <label htmlFor="Email"> Email </label>
+            <input
+              type="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <label htmlFor="Email"> Email </label>
+            <input
+              type="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <label htmlFor="Email"> Email </label>
+            <input
+              type="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </form>
+          <button onClick={openPopup}>Open Popup</button>
+          <button onClick={PrintEmailINColsole}>
+            {" "}
+            Print Email in console{" "}
+          </button>
+          {email !== "" && <p> Your email is: {email}</p>}
+        </div>
+      </div>
+      <div
+        className={`${
+          isOpen ? styles.popupEnterActive : styles.popupExitActive
+        }`}
+      >
+        <PopUp open={isOpen} onClose={closePopup} />
+      </div>
+    </>
+  );
+};
 
 export default Login;
